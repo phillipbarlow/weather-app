3ed6d13d69878fe4802d8582655cda5945a38eae
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2024-06-12 21:38:51 +0100

    top text positioned

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,34 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      
+      <input type="text" placeholder='Enter location'/>
+      <div className="container">
+        <div className="top">
+          <div className="temp">
+            <p>0C</p>
+          </div>
+          <div className="location">
+            <p>Leeds</p>
+          </div>
+          <div className="time">
+            <p>10:00</p>
+          </div>
+          <div className="condition">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="windSpeed">
+            <p>0 MPH</p>
+          </div>
+          <div className="feelsLike">
+            <p> Feels like 10C</p>
+          </div>
+          <div className="chanceOfRain">
+            <p>10%</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
