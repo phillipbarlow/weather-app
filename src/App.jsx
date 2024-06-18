6bfc12a0792f51a8a77a108554e9c8563beb21c8
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2024-06-18 21:26:03 +0100

    handling error on input

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import {getWeatherInfo} from './utils/api'
 function App() {
   const [input,setInput] = useState('');
   const [city,setCity] = useState('');
+  const [error, setError] = useState(null);
   function handleRequest(event){
           if(event.key === 'Enter'){
             event.preventDefault();
             return getWeatherInfo(input)
               .then((result)=>{
                 setCity(result);
+                setError(null);
                 console.log('city updated: ', result.data)
               })
               .catch((error)=>{
-                console.log(error)
+                if (error.response && error.response.status === 404) {
+                  setError('City not found');
+                } else {
+                  setError('An error occurred. Please try again.');
+                }
+                setCity('');
               })
             }
   }
@@ -23,11 +30,11 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="temp">
-            <p>0C</p>
+          {city && <p>{city.current.temp_c} C</p>}
           </div>
           <div className="location">
             {console.log('city updated: ', city)}
-            {city && <p>{city.location.name}</p>}
+            {error ? <p>{error}</p>:city && <p>{city.location.name}</p>}
           </div>
           <div className="time">
           {city && <p>{city.location.localtime}</p>}
